0f09d0fef67bcab58de93fecf08ddfc8093efe54
teamfairauction <94896055+teamfairauction@users.noreply.github.com>
2022-05-09 16:22:02 +0800

    Create ArticleTypeScopeEn.java

---
+++ src/commen/enums/ArticleTypeScopeEn.java
@@ -0,0 +1,14 @@
+package pub.developers.forum.common.enums;
+
+public enum ArticleTypeScopeEn {
+  
+    OFFICIAL("OFFICIAL", "官方"),
+    USER("USER", "用户"),
+    ;
+    private String value;
+    private String desc;
+
+    public static ArticleTypeScopeEn getEntity(String value) {
+       
+    }
+}
